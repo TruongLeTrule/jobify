1072e1abbe746a5c7a732cc512865f964e345de9
Truong Le <21522732@gm.uit.edu.vn>
2024-03-31 19:59:28 +0700

    add hash password function in register

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,13 @@
-import User from '../models/UserModel.js';
 import { StatusCodes } from 'http-status-codes';
+import User from '../models/UserModel.js';
+import { hashPassword } from '../utils/passwordUtils.js';
 
 export const register = async (req, res) => {
   const isFirstUser = (await User.countDocuments()) === 0;
   req.body.role = isFirstUser ? 'admin' : 'user';
-  const user = await User.create(req.body);
-  res.status(StatusCodes.CREATED).json({ user });
+  req.body.password = await hashPassword(req.body.password);
+  await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({ msg: 'user created' });
 };
 
 export const login = async (req, res) => {
